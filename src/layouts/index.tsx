df378a1ee39e643757291a946d7db0947b93439f
Seungbin Oh <sboh1214@gmail.com>
2020-08-02 16:59:23 +0900

    add google site verification

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -38,7 +38,8 @@ const IndexLayout: React.FC = ({ children }) => (
           htmlAttributes={{ lang: 'ko' }}
           meta={[
             { name: 'description', content: data.site.siteMetadata.description },
-            { name: 'keywords', content: data.site.siteMetadata.keywords }
+            { name: 'keywords', content: data.site.siteMetadata.keywords },
+            { name: 'google-site-verification', content: '5qAz7-fx88YJKFF95FsgcUq40tLfbycOgCjb08i2lw4' }
           ]}
         />
         <Header title={data.site.siteMetadata.title} />
