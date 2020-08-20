5a894caff22d58bd45f61b09a3db6625c14871f3
Seungbin Oh <sboh1214@gmail.com>
2020-08-20 20:45:05 +0900

    add back to top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,6 +8,9 @@ import ThemeContextProvider from './ThemeContext'
 import 'fontsource-roboto/index.css'
 import DrawerInner from './DrawerInner'
 import SearchAppBar from './SearchAppBar'
+import { Fab } from '@material-ui/core'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+import ScrollTop from './ScrollTop'
 
 const drawerWidth = 240
 const useStyles = makeStyles((theme: Theme) =>
@@ -49,48 +52,55 @@ export default function Layout({ children }: any) {
 
   return (
     <ThemeContextProvider>
-      <div className={classes.root}>
-        <CssBaseline />
-        <SearchAppBar drawerWidth={drawerWidth} onMenuClick={handleDrawerToggle} />
-        <nav className={classes.drawer} aria-label="mailbox folders">
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              container={container}
-              variant="temporary"
-              anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              ModalProps={{
-                keepMounted: true // Better open performance on mobile.
-              }}
-            >
-              <DrawerInner />
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              variant="permanent"
-              open
-            >
-              <DrawerInner />
-            </Drawer>
-          </Hidden>
-        </nav>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <article>{children}</article>
-          <footer>
-            <Footer />
-          </footer>
-        </main>
-      </div>
+      <>
+        <div className={classes.root}>
+          <CssBaseline />
+          <SearchAppBar drawerWidth={drawerWidth} onMenuClick={handleDrawerToggle} />
+          <nav className={classes.drawer} aria-label="mailbox folders">
+            {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+            <Hidden smUp implementation="css">
+              <Drawer
+                container={container}
+                variant="temporary"
+                anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                open={mobileOpen}
+                onClose={handleDrawerToggle}
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+                ModalProps={{
+                  keepMounted: true // Better open performance on mobile.
+                }}
+              >
+                <DrawerInner />
+              </Drawer>
+            </Hidden>
+            <Hidden xsDown implementation="css">
+              <Drawer
+                classes={{
+                  paper: classes.drawerPaper
+                }}
+                variant="permanent"
+                open
+              >
+                <DrawerInner />
+              </Drawer>
+            </Hidden>
+          </nav>
+          <main className={classes.content}>
+            <div className={classes.toolbar} id="back-to-top-anchor" />
+            <article>{children}</article>
+            <footer>
+              <Footer />
+            </footer>
+          </main>
+        </div>
+        <ScrollTop>
+          <Fab color="secondary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
+      </>
     </ThemeContextProvider>
   )
 }
