84d0f7d6db467c7e2b226acf0b12c3da8890dba4
Seungbin Oh <sboh1214@gmail.com>
2020-09-09 15:04:13 +0900

    sync

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -3,7 +3,8 @@ import { graphql } from 'gatsby'
 import Utterances from '../components/Utterances'
 import Root from '../components/Root'
 import 'prismjs/themes/prism-okaidia.css'
-import { Container, Typography } from '@material-ui/core'
+import { Container, Typography, CardContent, Card } from '@material-ui/core'
+import { MDXProvider } from '@mdx-js/react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 function Post({ data }) {
@@ -12,16 +13,24 @@ function Post({ data }) {
   return (
     <Root>
       <Container maxWidth="md">
-        <h1>{title}</h1>
-        <MDXRenderer
+        <Typography variant="h3">{title}</Typography>
+        <MDXProvider
           components={{
-            h1: (props) => <Typography variant="h1" {...props} />,
-            h2: (props) => <Typography variant="h1" {...props} />,
-            h3: () => <Typography variant="h1" />
+            h1: (props) => <Typography {...props} variant="h3" className={{ fontWeight: 'bold' }} />,
+            h2: (props) => <Typography variant="h4" {...props} />,
+            blockquote: (props) => (
+              <Card>
+                <CardContent>
+                  <Typography variant="body1" {...props} />
+                </CardContent>
+              </Card>
+            ),
+            table: (props) => <table className={{ width: '600px' }} {...props} />,
+            p: (props) => <Typography variant="body1" className={{ fontSize: '22px' }} {...props} />
           }}
         >
-          {body}
-        </MDXRenderer>
+          <MDXRenderer>{body}</MDXRenderer>
+        </MDXProvider>
         <Utterances />
       </Container>
     </Root>
