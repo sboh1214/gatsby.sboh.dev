1075126d30b88a2f9fdeeefdde9627122c12343c
Seungbin Oh <sboh1214@gmail.com>
2021-01-03 00:17:47 +0900

    Add menus

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@chakra-ui/react'
 import { Link } from 'gatsby'
 import React from 'react'
 import SbohLayout from 'sboh-layout'
@@ -8,12 +9,28 @@ type Props = {
   children: React.ReactNode
 }
 
-export default function Layout({ children }: Props) {
-  const site = useSiteMetadata()
-  const { menuLinks } = site.siteMetadata
-
+function Links() {
   return (
-    <SbohLayout title={<Link to="/">오승빈의 개발 블로그</Link>} links={<LinkList links={menuLinks} />}>
+    <>
+      <a href="https://sboh.dev">
+        <Button variant="ghost">About</Button>
+      </a>
+      <a href="https://sboh.dev/apps">
+        <Button variant="ghost">Apps</Button>
+      </a>
+      <Link to="/">
+        <Button variant="ghost">Blog</Button>
+      </Link>
+      <a href="https://sboh.dev/libs">
+        <Button variant="ghost">Libraries</Button>
+      </a>
+    </>
+  )
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <SbohLayout title={<Link to="/">오승빈의 개발 블로그</Link>} links={<Links />}>
       {children}
     </SbohLayout>
   )
