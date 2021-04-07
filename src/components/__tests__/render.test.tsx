fd6e8253a9c4c5d99bb70ed40db6e4573f7d9a45
Seungbin Oh <sboh1214@gmail.com>
2021-04-07 18:47:25 +0900

    Remove blog on site

--- src/components/__tests__/render.test.tsx
+++ src/components/__tests__/render.test.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Layout from '../layout'
-import CategoryFilter from '../categoryFilter'
-
-test('render', async () => {
-  render(<CategoryFilter categoryList={[{ fieldValue: '', totalCount: 1 }]} />)
-})
 
 test('render', async () => {
   render(<Layout title="Hello">Hello</Layout>)
